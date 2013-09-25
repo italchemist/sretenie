004881ef05cc47f783d8ddf88c195e7ffa2994db
Alexey Leontiev <me@italchemist.org>
2013-09-25 19:51:32 +0400

    email

---
+++ app/mailers/request_mailer.rb
@@ -0,0 +1,8 @@
+class RequestMailer < ActionMailer::Base
+  default from: "larisa_1956@mail.ru"
+
+  def request_email(params)
+  	@params = params
+  	mail(to: "braincube@yandex.ru", subject: "Заявка")
+  end
+end
