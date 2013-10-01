3789319f1b612fac0fb3b94b63ac01eeb3cd0054
Alexey Leontiev <me@italchemist.org>
2013-10-01 20:46:38 +0400

    email

--- app/admin/trip_schedule.rb
+++ app/admin/trip_schedule.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register TripSchedule do
   controller do
     def permitted_params
-      params.permit(:trip_schedule => [:date_start, :date_end, :price])
+      params.permit(:trip_schedule => [:date_start, :date_end, :price, :trip_id])
     end
   end
 end
