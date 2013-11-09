53b945beb56a3f2002c2e3bf2ec04668d6b7ba89
Alexey Leontiev <me@italchemist.org>
2013-11-09 12:52:24 +0400

    mailer

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -4,6 +4,10 @@ class RequestMailer < ActionMailer::Base
   default from: "request@sreteniespb.ru"
 
   def request_email(params)
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	@params = params
   	mail(to: "request@sreteniespb.ru", subject: "Заявка")
   end
 end
