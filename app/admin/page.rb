67b34bb6312ac3162b7d2e67671e8e25bece5cc1
Alexey Leontiev <me@italchemist.org>
2013-11-12 19:42:39 +0400

    pages

--- app/admin/page.rb
+++ app/admin/page.rb
@@ -4,4 +4,14 @@ ActiveAdmin.register Page do
       params.permit!
     end
   end
+
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :url 
+      f.input :content, as: :html_editor
+    end
+
+    f.buttons
+  end
 end
